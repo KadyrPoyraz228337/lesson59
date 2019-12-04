0bedbc8080dac789dfbd46a4430760af5b0d2bab
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-12-04 12:33:36 +0200

    Добавил state в App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
+import nanoid from 'nanoid';
+import Container from "reactstrap/es/Container";
+
+import Form from "./components/Form/form";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 class App extends Component {
+    state = localStorage.getItem('movies') ? JSON.parse(localStorage.getItem('movies')) : {
+      movies: [
+          {title: 'Movie name', id: nanoid()},
+          {title: 'Movie name', id: nanoid()},
+      ]
+    };
     render() {
         return (
-            <div>
-
-            </div>
+            <Container>
+                <Form/>
+            </Container>
         );
     }
 }
